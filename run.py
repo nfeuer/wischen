40aeaa0e9a6e69c7f5585f7f2b528cb14bb1ae64
Breichman <br@rgroup2003.com>
2017-11-11 17:16:46 -0400

    So far

--- run.py
+++ run.py
@@ -37,7 +37,5 @@ class HotelList(Resource):
 api.add_resource(HotelList, '/')
 
 if __name__ == '__main__':
-    h = HotelList()
-    print(h.post())
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
